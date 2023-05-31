9065d38c560c381910cb1c7b1d07c794909b0071
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-05-31 11:44:07 +0500

    notString done

--- notString.js
+++ notString.js
@@ -1,19 +1,11 @@
 const notString = (str) => {
-    if(str === 'not'){
-        return str;
-      }
-      
-      else if(str){
-        return "not " + str;
-      }
-      
-      else if(str.match(/not/)){
-        return str; 
-      }
-      
-      else {
-        return str;
-      }
+  if(str.startsWith('not')){
+    return str;
+  }
+
+  else{
+    return "not " + str;
+  }
 }
 
 console.log(notString('is not'));
