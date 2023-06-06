af6b1ba55aacc813bc5507f0d2503fcedf197b37
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-06-06 11:56:50 +0500

    intMax done

---
+++ intMax.js
@@ -0,0 +1,15 @@
+let intMax = (a, b, c) => {
+    if(a > b && a > c){
+        return a
+    }
+
+    if(b > a && b > c){
+        return b
+    }
+
+    else{
+        return c
+    }
+}
+
+console.log(intMax(1,12,23));
